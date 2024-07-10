f356a42aafb233723a398f1d9fe880ec6fb4e8d1
Seungjin <dosjin01@gmail.com>
2024-07-10 14:21:05 +0900

    [도승진]게시글 등록

---
+++ src/main/java/com/gn/user/controller/UserLogoutServlet.java
@@ -0,0 +1,34 @@
+package com.gn.user.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/user/logout")
+public class UserLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public UserLogoutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 로그아웃은 로그인이 완성되야 할 수 있다
+		HttpSession session = request.getSession(false);
+		if(session != null && session.getAttribute("user")!=null) {
+			session.removeAttribute("user");
+			session.invalidate();
+		}
+		response.sendRedirect("/");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
